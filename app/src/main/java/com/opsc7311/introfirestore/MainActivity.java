914e92a55987d359172f189fa8252ec9ec2b345f
Denzel-Witbooi <dwitbooi28@gmail.com>
2022-04-18 15:29:45 +0200

    Reading and writing to firestore

--- app/src/main/java/com/opsc7311/introfirestore/MainActivity.java
+++ app/src/main/java/com/opsc7311/introfirestore/MainActivity.java
@@ -10,9 +10,14 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 
 import java.util.HashMap;
@@ -41,31 +46,94 @@ public class MainActivity extends AppCompatActivity {
 
         Button saveButton = findViewById(R.id.save_button);
 
-        saveButton.setOnClickListener(view -> {
-            String title =  enterTitle.getText().toString().trim();
-            String thought = enterThought.getText().toString().trim();
+//        saveButton.setOnClickListener(view -> {
+//            String title =  enterTitle.getText().toString().trim();
+//            String thought = enterThought.getText().toString().trim();
+//
+//            Map<String, Object> data = new HashMap<>();
+//            data.put(KEY_TITLE, title);
+//            data.put(KEY_THOUGHT,thought);
+//
+//            db.collection("Journal")
+//                    .document("First Thoughts")
+//                    .set(data)
+//                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                        @Override
+//                        public void onSuccess(Void unused) {
+//                            Toast.makeText(MainActivity.this,
+//                                    "Success", Toast.LENGTH_LONG).show();
+//                        }
+//                    })
+//                    .addOnFailureListener(new OnFailureListener() {
+//                        @Override
+//                        public void onFailure(@NonNull Exception e) {
+//                            Log.d(TAG, "onFailure: " + e);
+//                        }
+//                    });
+//
+//        });
+        /**
+         * Add DATA
+         */
+        // Create a new user with a first and last name
+//        Map<String, Object> user = new HashMap<>();
+//        user.put("first","Ada");
+//        user.put("last","Lovelace");
+//        user.put("born", 1815);
+//
+//        // Add a new document with a generated ID
+//        db.collection("users")
+//                .add(user)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Log.d(TAG, "DocumentSnapshot add with ID: " + documentReference.getId());
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(TAG, "Error adding document ", e);
+//                    }
+//                });
 
-            Map<String, Object> data = new HashMap<>();
-            data.put(KEY_TITLE, title);
-            data.put(KEY_THOUGHT,thought);
+        // Create a new user with a first, middle, and last name
+//        Map<String, Object> user = new HashMap<>();
+//        user.put("first","Alan");
+//        user.put("middle", "Mathison");
+//        user.put("last","Turing");
+//        user.put("born",1912);
+//
+//        // Add a new document with a generated ID
+//        db.collection("users")
+//                .add(user)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(TAG, "Error adding document", e);
+//                    }
+//                });
 
-            db.collection("Journal")
-                    .document("First Thoughts")
-                    .set(data)
-                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void unused) {
-                            Toast.makeText(MainActivity.this,
-                                    "Success", Toast.LENGTH_LONG).show();
+        // Read Data
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents. ", task.getException());
                         }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.d(TAG, "onFailure: " + e);
-                        }
-                    });
-
-        });
+                    }
+                });
     }
 }
